28b8938fa66173f2573fbb86c90fa6be907de88a
Robin te Hofstee <robintehofstee@gmail.com>
2026-01-28 11:59:01 -0500

    fixed random quotes

--- components/QuoteDisplay.tsx
+++ components/QuoteDisplay.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import quotes from '@/data/Quotes'; // Adjust the path
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import quotes from '@/data/Quotes';
 
 const QuoteDisplay = () => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const { quote, author } = quotes[randomIndex];
+  const [quote, setQuote] = useState({ quote: '', author: '' });
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  }, []);
+
+  if (!quote.quote) return null; // Prevent flash of empty content
 
   return (
     <div>
-      <p className="text-md italic">"{quote}"</p>
-      <p className="text-sm">- {author}</p>
+      <p className="text-md italic">"{quote.quote}"</p>
+      <p className="text-sm">- {quote.author}</p>
     </div>
   );
 };
 
 export default QuoteDisplay;
-
